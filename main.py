226ff7c1664d908a8a795422d931dbc62abfff7e
kaustubhpatilkp <kaustubhpatil26@gmail.com>
2024-07-31 00:30:04 +0530

    Day1

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':'blog list'}
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data':f'{limit} published blogs from db'}
+    else:
+        return {'data':f'{limit} blogs from db'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'All unpublished blogs'}
 
 @app.get('/blog/{id}')
-def show(id):
+def show(id: int): #pydantic
     return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':{'1','2'}}
+
